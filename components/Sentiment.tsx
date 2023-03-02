b4a4de4a3ea038a3f214c9256208af2c0670155b
Calvin Low <calsaviour@gmail.com>
2023-03-02 20:04:48 +0800

    Add text colour to sentiment component

--- components/Sentiment.tsx
+++ components/Sentiment.tsx
@@ -8,19 +8,19 @@ const Sentiment = ({ score }: SentimentProps) => {
     console.log(parseFloat(score));
     if(parseFloat(score) < -0.2) {
         return (
-            <p>
+            <p className="text-left font-medium text-red-500">
                 Sentiment Score: {score} (Negative)
             </p>
         )
     } else if (parseFloat(score) > 0.2) {
         return (
-            <p>
+            <p className="text-left font-medium text-cyan-500">
                 Sentiment Score: {score} (Positive)
             </p>
         )
     } else {
         return(
-            <p>
+            <p className="text-left font-medium">
                 Sentiment Score: {score} (Neutral)
             </p>
         )
