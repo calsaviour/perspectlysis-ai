21bea76f66ac330b861d0a2537b0c9dbb6e718c1
Calvin Low <calsaviour@gmail.com>
2023-04-01 19:18:15 +0800

    Add input box for openai apikey

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -23,7 +23,7 @@ export interface OpenAIStreamPayload {
   n: number;
 }
 
-export async function OpenAIStream(payload: OpenAIStreamPayload) {
+export async function OpenAIStream(payload: OpenAIStreamPayload, apiKey: string) {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
@@ -32,7 +32,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      Authorization: `Bearer ${apiKey ?? ""}`,
     },
     method: "POST",
     body: JSON.stringify(payload),
